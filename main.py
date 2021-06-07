56ffcc4f7b569ad8fa52dca7bef0bdca743be5a6
isabella3412 <>
2021-06-07 13:55:20 +0000

    completed problems 1 and 7

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-
-
 def problem1():
-  # Complete problem 1 here
-  pass
+  filename = "inputs/Prob01.in.txt"
+  with open(filename) as f:
+    for line in f:
+      print(line.rstrip("\n"))
+      number = int(line.rstrip("\n"))
+      #what to do with each number
+      if number >= 70:
+        print ("PASS")
+      elif number < 70:
+        print ("FAIL")
 
 problem1()
+
+def problem7():
+  filename = "inputs/Prob07.in.txt"
+  with open(filename) as f:
+    for line in f:
+      print(line.rstrip("\n") + " is a palindrome?") 
+      s = str(line.rstrip("\n"))
+      stringlength = len(s) 
+      reversedString = s[stringlength::-1] 
+      if s == reversedString:
+        print ("True")
+      elif s != reversedString: 
+        print ("False")
+        
+problem7()
